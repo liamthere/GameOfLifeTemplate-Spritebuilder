5662e00978a0dee6caed13b473924b7f4396ad4a
Liam Lehr <liam@Lexs-MacBook-Air.local>
2014-07-17 20:15:43 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -86,4 +86,15 @@ static const int GRID_COLUMNS = 10;
     return _gridArray[row][column];
 }
 
+-(void)evolveStep {
+    //update each Creature's neighbor count
+    [self countNeighbors];
+    
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+}
+
 @end
